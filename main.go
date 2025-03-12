38f7150f620aa57b36ec342c4f8a60de466ca8d8
Alexander Hansen <ahansen1335@gmail.com>
2025-03-12 06:21:12 +0100

    read the file line by line

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,12 +14,35 @@ func main() {
 		return
 	}
 	buffer := make([]byte, 8)
+
+	currentLine := ""
+
 	for {
 		read, err := messages.Read(buffer)
 		if err != nil {
+			if err == io.EOF && currentLine == "" {
+				fmt.Printf("read: %s\n", currentLine)
+			}
 			return
 		}
-		fmt.Printf("read: %s\n", buffer[:read])
 
+		text := string(buffer[:read])
+
+		parts := strings.Split(text, "\n")
+
+		for i := 0; i < len(parts)-1; i++ {
+			currentLine += parts[i]
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = ""
+		}
+
+		if len(text) > 0 && text[len(text)-1] == '\n' {
+			currentLine += parts[len(parts)-1]
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = ""
+		} else {
+			currentLine += parts[len(parts)-1]
+
+		}
 	}
 }
