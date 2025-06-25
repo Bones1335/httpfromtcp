e1b55189745e1f3143d12baf890881c0368b44f4
Alexander Hansen <ahansen1335@gmail.com>
2025-06-25 06:35:07 +0200

    test body parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -35,6 +35,8 @@ func main() {
 		for k, v := range r.Headers {
 			fmt.Printf("- %+v: %+v\n", k, v)
 		}
+		fmt.Println("Body:")
+		fmt.Printf("%s\n", r.Body)
 
 		conn.Close()
 		fmt.Println("Connection closed......")
